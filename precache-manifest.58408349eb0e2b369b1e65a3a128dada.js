3850d7c302f91e91306dd0ec92fda1c1f9c1db34
tonytang731 <50162066+tonytang731@users.noreply.github.com>
2020-07-27 00:51:58 -0500

    Updates

---
+++ precache-manifest.58408349eb0e2b369b1e65a3a128dada.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa485de0d24f3831f712a113f6d0aa2",
+    "url": "/github-pages-react-app/index.html"
+  },
+  {
+    "revision": "876e503e57799d8da189",
+    "url": "/github-pages-react-app/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e841e4e4540ae300b688",
+    "url": "/github-pages-react-app/static/css/main.220cfeea.chunk.css"
+  },
+  {
+    "revision": "876e503e57799d8da189",
+    "url": "/github-pages-react-app/static/js/2.6cb597db.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/github-pages-react-app/static/js/2.6cb597db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e841e4e4540ae300b688",
+    "url": "/github-pages-react-app/static/js/main.3ed6649e.chunk.js"
+  },
+  {
+    "revision": "6b86c044deb4f4deb8b7",
+    "url": "/github-pages-react-app/static/js/runtime-main.60790d88.js"
+  },
+  {
+    "revision": "33e98c73d80a5e93ebe5cab30f8a6b0f",
+    "url": "/github-pages-react-app/static/media/face.33e98c73.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage-react-app/precache-manifest.931abecc9e076eed6eeec2a2a0a949d5.js"
+  "/github-pages-react-app/precache-manifest.58408349eb0e2b369b1e65a3a128dada.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-pages-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
