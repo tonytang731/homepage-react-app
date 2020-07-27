8f345c6b39786fa60d2abb60e4fea48101b4480d
tonytang731 <50162066+tonytang731@users.noreply.github.com>
2020-07-26 23:53:51 -0500

    final version 3

--- src/Components/CV.js
+++ src/Components/CV.js
@@ -3,8 +3,8 @@ class CV extends Component {
     render() {
         return (
             <div className='pdf'>
-                <a href="/static/media/Yiwen_CV_Eng.70c2b73b.pdf" class="image fit"><img src="images/marr_pic.jpg" alt=""/>Download Yiwen's CV PDF (English Version)</a><br></br>
-                <a href="/static/media/Yiwen_CV_Chn.53bf2d9c.pdf" class="image fit"><img src="images/marr_pic.jpg" alt=""/>Download Yiwen's CV PDF (Chinese Version)</a><br></br>
+                <a href="/static/media/Yiwen_CV_Eng.pdf" class="image fit"><img src="images/marr_pic.jpg" alt=""/>Download Yiwen's CV PDF (English Version)</a><br></br>
+                <a href="/static/media/Yiwen_CV_Chn.pdf" class="image fit"><img src="images/marr_pic.jpg" alt=""/>Download Yiwen's CV PDF (Chinese Version)</a><br></br>
             </div>
         )
     }
